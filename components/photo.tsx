07c7fdb9231387938eb53850f711dc5520af0da0
Dimitrij Agal <dimitrij@traveloka.com>
2020-07-13 21:31:10 +0700

    fix

--- components/photo.tsx
+++ components/photo.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-export default function Photo({ name, spacing }) {
+export default function Photo({ name }) {
   return (
     <Link href='/hero/[heroName]' passHref as={`/hero/${name.toLowerCase()}`}>
-      <a className='photolink' aria-label={`Link for opening hero detail for ${name}`} style={{ marginLeft: spacing, marginRight: spacing }}>
+      <a className='photolink' aria-label={`Link for opening hero detail for ${name}`}>
         <div className='w-24 h-24 photo'>
           <img src={`/img/face/${name}.png`} />
         </div>
