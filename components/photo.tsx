0dfd7c0615f6a68a2b6bd8f6db67a70f6255285e
Dimitrij Agal <dimitrij@traveloka.com>
2020-07-13 22:40:04 +0700

    user select none

--- components/photo.tsx
+++ components/photo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Photo({ name }) {
   return (
     <Link href='/hero/[heroName]' passHref as={`/hero/${name.toLowerCase()}`}>
-      <a className='photolink' aria-label={`Link for opening hero detail for ${name}`}>
+      <a className='photolink select-none' aria-label={`Link for opening hero detail for ${name}`}>
         <div className='w-24 h-24 photo'>
           <img src={`/img/face/${name.toLowerCase()}.png`} />
         </div>
